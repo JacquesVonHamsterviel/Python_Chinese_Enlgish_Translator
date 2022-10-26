3d730783afe4dc7768b87fc9a85c2857214ee413
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-10-26 20:44:19 +0100

    Update test_base64.py

--- test_base64.py
+++ test_base64.py
@@ -1,7 +1,7 @@
 def base64_translate(q):
   verify_key=str(random.randint(100000000,999999999))
   data={'text':base64.b64encode(q.encode()),'lan':'en','verify_key':verify_key}
-  r=requests.post("http://106.14.143.32:5690/base64", data, headers=user_agent, timeout=60)
+  r=requests.post("http://127.0.0.1:5690/base64", data, headers=user_agent, timeout=60)
   print(r.content.decode('utf-8'))
   #time.sleep(1)
   if verify_key in base64.b64decode(r.content).decode():
